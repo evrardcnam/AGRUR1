c2a52e236e395a6cb34cbefdc37b90db1f59ebb9
Lucidiot <erwanrct@gmail.com>
2016-11-24 18:15:56 +0100

    Gestion des lots dans les commandes

--- php/class/commande.php
+++ php/class/commande.php
@@ -5,21 +5,37 @@ class Commande
     //données privées de la classe
     private $_numCommande;
     private $_dateEnvoie;
+    private $_nomClient;
+    private $_codeLot;
     // Constructeur de la classe depuis la couche d'accès aux données
     public function __construct(DBQueryResult $result)
     {
         $this->_numCommande = $result->numCommande;
         $this->_dateEnvoie = $result->dateEnvoie;
+        $this->_nomClient = $result->nomClient;
+        $this->_codeLot = $result->codeLot;
     }
     // Accesseur
     public function __get($var)
     {
         switch ($var) {
-            case '_num':
+            case 'num':
                 return $this->_numCommande;
                 break;
-            case '_dateEnvoie':
+            case 'dateEnvoie':
                 return $this->_dateEnvoie;
+                break;
+            case 'nomClient':
+                return $this->_nomClient;
+                break;
+            case 'client':
+                return getClient($_nomClient);
+                break;
+            case 'codeLot':
+                return $this->_codeLot;
+                break;
+            case 'lot':
+                return getLotCommande($_numCommande);
                 break;
             default:
                 return null;
--- php/class/db_layer.php
+++ php/class/db_layer.php
@@ -1,23 +1,23 @@
 <?php
 // Résultat de requête SELECT.
 class DBQueryResult {
-private $_results = array();
-public function __construct() {}
-public function __set($var,$val) {
-$this->_results[$var] = $val;
-}
-public function __get($var){
-if (isset($this->_results[$var])) {
-return $this->_results[$var];
-} else { return null; }
-}
+	private $_results = array();
+	public function __construct() {}
+	public function __set($var,$val) {
+		$this->_results[$var] = $val;
+	}
+	public function __get($var){
+		if (isset($this->_results[$var])) {
+			return $this->_results[$var];
+		} else { return null; }
+	}
 }
 // Décrit une couche d'accès aux données.
 class DBLayer {
 	public function __construct() {}
 
 	// Obtenir tous les producteurs
-	public function getProducteurs() {
+	public static function getProducteurs() {
 		$results = $this->query("SELECT * FROM Producteur ORDER BY nomProducteur ASC");
 		if (!$results) { return $results; }
 		else {
@@ -30,17 +30,14 @@ class DBLayer {
 	}
 
 	// Obtient un producteur spécifique.
-	public function getProducteur(string $nom) {
-		$results = $this->query("SELECT * FROM Producteur WHERE nomProducteur LIKE " . $nom);
+	public static function getProducteur(string $nom) {
+		$results = $this->query("SELECT * FROM Producteur WHERE nomProducteur LIKE " . $nom . " LIMIT 0,1");
 		if (!$results) { return null; }
-		else {
-			$object_results = array();
-			return new Producteur($results[0]);
-		}
+		else { return new Producteur($results[0]); }
 	}
 
 	// Obtenir tous les clients
-	public function getClients() {
+	public static function getClients() {
 		$results = $this->query("SELECT * FROM Client ORDER BY idClient ASC");
 		if (!$results) { return $results; }
 		else {
@@ -52,8 +49,15 @@ class DBLayer {
 		}
 	}
 
+	// Obtenir un client par son nom
+	public static function getClient(string $nom) {
+		$results = $this->query("SELECT * FROM Client WHERE nomClient LIKE " . $nom . " LIMIT 0,1");
+		if (!$results) { return null; }
+		else { return new Client($results[0]); }
+	}
+
 	// Obtenir toutes les certifications
-	public function getCertifications() {
+	public static function getCertifications() {
 		$results = $this->query("SELECT * FROM Certification ORDER BY libelleCertification ASC");
 		if (!$results) { return $results; }
 		else {
@@ -66,7 +70,7 @@ class DBLayer {
 	}
 	
 	// Obtenir toutes les commandes
-	public function getCommandes() {
+	public static function getCommandes() {
 		$results = $this->query("SELECT * FROM Commande ORDER BY numCommande ASC");
 		if (!$results) { return $results; }
 		else {
@@ -79,7 +83,7 @@ class DBLayer {
 	}
 
 	// Obtenir toutes les certifications validées pour un producteur
-	public function getCertificationsValidees(Producteur $p) {
+	public static function getCertificationsValidees(Producteur $p) {
 		$results = $this->query("SELECT * FROM Certification C, Obtient O WHERE O.idCertification = C.idCertification AND O.nomProducteur LIKE " . $p->nom . " ORDER BY libelleCertification ASC");
 		if (!$results) { return $results; }
 		else {
@@ -91,8 +95,15 @@ class DBLayer {
 		}
 	}
 
+	// Obtenir le lot d'une commande
+	public static function getLotCommande(Commande $c) {
+		$results = $this->query("SELECT * FROM Lot WHERE numCommande = " . $c->num . " LIMIT 0,1");
+		if (!$results) { return null; }
+		else { return new Lot($results[0]); }
+	}
+
 	// Connexion à la base de données. Action réalisée par les autres fonctions internes.
-	private function dbconnect() {
+	private static function dbconnect() {
 		$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DB);
 		if ($conn->connect_error) {
 			die('Erreur de connexion (' . $conn->connect_errno . ') ' . $conn->connect_error);
@@ -101,7 +112,7 @@ class DBLayer {
 	}
 	
 	// Exécuter une requête sur la base de données.
-	private function query(string $sql) {
+	private static function query(string $sql) {
 		$this->dbconnect();
 		$res = mysql_query($sql);
 		if ($res) {
