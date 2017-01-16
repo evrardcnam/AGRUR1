a09b92b660e942be05e3769fdecd737bc36fc863
Lucidiot <erwanrct@gmail.com>
2017-01-16 13:37:08 +0100

    Réorganisation

--- masterClient.php
+++ masterClient.php
@@ -18,9 +18,9 @@
 						<a class="navbar-brand" href="#">AGRUR </a>
 					</div>
 					<ul class="nav navbar-nav">
-						<li><a id="home" class="slavePage" data-link="php/accueilClient.php">Accueil</a></li>
-						<li><a class="slavePage" data-link="php/lots.php">Lots</a></li>
-						<li><a class="slavePage" data-link="php/commandes.php">Commandes</a></li>
+						<li><a id="home" class="slavePage" data-link="php/client/accueil.php">Accueil</a></li>
+						<li><a class="slavePage" data-link="php/client/lots.php">Lots</a></li>
+						<li><a class="slavePage" data-link="php/client/commandes.php">Commandes</a></li>
 					</ul>
 					<ul class="nav navbar-nav pull-right">
 						<li><a href="javascript:logout();"><span class="glyphicon glyphicon-off"></span></a></li>
